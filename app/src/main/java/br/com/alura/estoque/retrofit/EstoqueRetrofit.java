5e225417ee9f872a4c11af2844c7fa77b080a37c
pedrindenard <pedrominuzzi21@gmail.com>
2021-01-28 10:31:46 -0300

    Utilizando callback na busca de produtos

--- app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -19,7 +19,7 @@ public class EstoqueRetrofit {
                 .build();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.20.249:8080/")
+                .baseUrl("http://192.168.2.113:8080/")
                 .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
