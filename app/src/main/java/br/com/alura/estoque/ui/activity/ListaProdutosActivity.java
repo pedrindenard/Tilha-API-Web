1443b458f48bc753373d14d96e4f81318920f23a
pedrindenard <pedrominuzzi21@gmail.com>
2021-01-29 08:19:12 -0300

    Integrando omportamento de edição

--- app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
+++ app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
@@ -96,16 +96,17 @@ public class ListaProdutosActivity extends AppCompatActivity {
 
     private void abreFormularioEditaProduto(int posicao, Produto produto) {
         new EditaProdutoDialog(this, produto,
-                produtoEditado -> edita(posicao, produtoEditado))
-                .mostra();
-    }
+                produtoEditado -> repository.edita(produtoEditado, new ProdutoRepository.DadosCarregadosCallback<Produto>() {
+                    @Override
+                    public void quandoSucesso(Produto produtoEditado) {
+                        adapter.edita(posicao, produtoEditado);
+                    }
 
-    private void edita(int posicao, Produto produto) {
-        new BaseAsyncTask<>(() -> {
-            dao.atualiza(produto);
-            return produto;
-        }, produtoEditado ->
-                adapter.edita(posicao, produtoEditado))
-                .execute();
+                    @Override
+                    public void quandoFalha(String erro) {
+                        Toast.makeText(ListaProdutosActivity.this, "Nâo fooi possivel editar o produto", Toast.LENGTH_SHORT).show();
+                    }
+                }))
+                .mostra();
     }
 }
